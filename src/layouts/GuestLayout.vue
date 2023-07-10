b924e1a07c69bd2417467e8f5eadda9e46bc4a9d
1301304 <xtianmarilla@gmail.com>
2023-07-10 16:29:41 +0800

    partially complete UI

--- src/layouts/GuestLayout.vue
+++ src/layouts/GuestLayout.vue
@@ -1,60 +1,15 @@
+<script setup>
+import MainHeader from "components/MainHeader.vue";
+</script>
+
 <template>
-  <q-layout view="hHh lpR fFf">
-    <q-header elevated class="bg-primary text-white" height-hint="98">
-      <q-toolbar>
-        <q-toolbar-title>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
-          </q-avatar>
-          Title
-        </q-toolbar-title>
-
-        <q-btn dense flat round icon="menu" @click="toggleRightDrawer" />
-      </q-toolbar>
-
-      <q-tabs align="left">
-        <q-route-tab to="/login" label="Guest" />
-        <q-route-tab to="/patient" label="Patient's Page" />
-        <q-route-tab to="/appointment" label="Appointments" />
-        <q-route-tab to="/consultation" label="Consultation" />
-        <q-route-tab to="/chat" label="Chat's Page" />
-      </q-tabs>
+  <q-layout view="hHh Lpr fFf">
+    <q-header bordered class="doc-header header-toolbar doc-brand">
+      <MainHeader />
     </q-header>
 
-    <q-drawer show-if-above v-model="rightDrawerOpen" side="right" bordered>
-      <!-- drawer content -->
-    </q-drawer>
-
-    <q-page-container>
+    <q-page-container class="doc-technical">
       <router-view />
     </q-page-container>
-
-    <q-footer elevated class="bg-grey-8 text-white">
-      <q-toolbar>
-        <q-toolbar-title>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
-          </q-avatar>
-          <div>Title</div>
-        </q-toolbar-title>
-      </q-toolbar>
-    </q-footer>
   </q-layout>
 </template>
-
-<script>
-import { ref } from "vue";
-
-export default {
-  setup() {
-    const rightDrawerOpen = ref(false);
-
-    return {
-      rightDrawerOpen,
-      toggleRightDrawer() {
-        rightDrawerOpen.value = !rightDrawerOpen.value;
-      },
-    };
-  },
-};
-</script>

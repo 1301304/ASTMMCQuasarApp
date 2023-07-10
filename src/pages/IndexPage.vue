78cc9e96e2ced99c82e5266d06f1172141b56747
1301304 <xtianmarilla@gmail.com>
2023-07-10 11:41:41 +0800

    Layout

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,26 @@
 <template>
+  <div class="q-pa-md">
+    <div class="q-video">
+      <iframe
+        src="https://www.youtube.com/embed/fuhE6PYnRMc"
+        frameborder="0"
+        allowfullscreen
+      />
+    </div>
+  </div>
   <q-page class="flex flex-center">
     <img
       alt="Quasar logo"
       src="~assets/quasar-logo-vertical.svg"
       style="width: 200px; height: 200px"
-    >
+    />
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'IndexPage'
-})
+  name: "IndexPage",
+});
 </script>
